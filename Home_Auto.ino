d21d0bfc43cc17ef27e2fb0589aed00708a9ccdb
Aravind <aravindma@gmail.com>
2013-02-01 00:27:32 +0530

    Added code framework and TODO things

---
+++ Home_Auto.ino
@@ -0,0 +1,32 @@
+/*****************DEFINE GLOBAL VARIABLES HERE*****************************/
+
+//TODO :include global variables used in your respective functions so that other functions can also use them. Example RTC.date
+
+/**************************************************************************/
+
+/*****************FUNCTION DEFINITIONS*************************************/
+
+//TODO: your actual functions defined here. We need the following
+
+//RTC_update(If there is any update of time to be done in case of battery failures)
+//RTC_check (to check if the time has reached for an action, and return the relay updated positions if any changes)
+//Relay_update(if above function returns a positive,the affect the relays as required)
+//LCD_Menu(funtion with all the menu possiblities)
+//Button_Interuppt(What to do when a button is pressed for the LCD)
+
+/*****************MASTER SETUP FUNCTION***********************************/
+
+void setup()
+{
+  //TODO : only main setups here.do not include your function setups.If the functions need to be setup here itself then just call the respective funtion to keep it clean.
+}
+
+/****************MASTER LOOP FUNTION***************************************/
+ void loop()
+{
+  //TODO: Just call your respective funtions here and update the global variables as and when required. That way its independant of each other's code.
+
+}
+
+
+//ASSUME constant values if the code for that part isn't updated yet.
