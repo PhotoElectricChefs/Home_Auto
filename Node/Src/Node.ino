a0d5afe6e6c055278133004bfe5fe990e33e1e2f
aravindma1990 <aravindma1990@gmail.com>
2013-06-22 17:57:04 +0530

    Added basic flow structure with example

--- Node/Src/Node.ino
+++ Node/Src/Node.ino
@@ -1,6 +1,34 @@
-void setup()
+/************Public Includes************************/
+
+
+
+/***************************************************/
+/************Private Variables**********************/
+
+
+/***************************************************/
+/************Private Function Declarations**********/
+void Node_Init();
+void Read_EEPROM();
+
+/***************************************************/
+/***********Function Defintions*********************/
+void Node_Init()
 {
-		//Initialization
+	Read_EEPROM(); //Read EEPROM to determine previous states and properties of the switches
+}
+
+void Read_EEPROM();
+{
+	//TODO:add code here to read previous state from eeprom
+}
+/***************************************************/
+
+
+
+void setup() //Initialization
+{
+	Node_Init();	//Initialize node to previous state. 	
 }
 
 void loop()
