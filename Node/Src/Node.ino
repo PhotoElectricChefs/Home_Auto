a1f02532b81e6f7b1a7ef31b7ee6219bca6d4e85
Hussain <moihussain@gmail.com>
2013-06-27 00:37:36 +0530

    More comments to make it readable

--- Node/Src/Node.ino
+++ Node/Src/Node.ino
@@ -7,7 +7,8 @@
 #define N 4
 //Defining pins on Arduino through which switches are controlled
 
-/*#define SWITCH1_PIN 8
+/*
+#define SWITCH1_PIN 8
 #define SWITCH2_PIN 9
 #define SWITCH3_PIN 10
 #define SWITCH4_PIN 11
@@ -19,20 +20,25 @@ const unsigned short int switch_pins[N]={8,9,10,11};	//for 4 switch node
    Assuming 20 bytes would suffice for each switch data. Can extend if needed.
    Here addresses 20-39 : SWITCH1, 40-59 : SWITCH2, 60-79 : SWITCH3, 80-99 : SWITCH4 	*/
 
-/*#define SWITCH1_EEPROM_START_ADDR 20
+/*
+#define SWITCH1_EEPROM_START_ADDR 20
 #define SWITCH2_EEPROM_START_ADDR 40
 #define SWITCH3_EEPROM_START_ADDR 60
 #define SWITCH4_EEPROM_START_ADDR 80
 */
+
 const unsigned short int switch_addr[N]={20,40,60,80};	//for 4 switch node 
+
 /***************************************************/
 /************Private Variables**********************/
+
 unsigned short int i;					//index variable
+
 //Every switch has a pin,state and EEPROM start address associated with it
 struct switches
 {
 unsigned short int pin;
-boolean state;
+boolean state;						//true=ON, false=OFF
 byte addr;
 };
 struct switches S[N];					// switches: S[0],S[1],S[2],...S[N-1]
@@ -56,6 +62,7 @@ void Node_Init()
 
 //Read EEPROM to determine previous states and properties of the switches
 	Read_EEPROM(); 		   
+
 //restore switches to previous state
 	for(i=0;i<N;i++)
 	status_update(i,S[i].state); 
@@ -77,9 +84,9 @@ void status_update(unsigned short int switch_number, boolean switch_state)
 {
 /*Usage Example: status_update(0, true) to turn ON switch S0*/
 	
-		 digitalWrite(S[switch_number].pin, (switch_state==true)?HIGH:LOW);
-		 S[switch_number].state=switch_state;
-		 EEPROM.write(S[switch_number].addr, S[switch_number].state);
+		 digitalWrite(S[switch_number].pin, (switch_state==true)?HIGH:LOW);	//send control signal to switch ON/OFF a switch
+		 S[switch_number].state=switch_state;					//update state in switch's element in structure
+		 EEPROM.write(S[switch_number].addr, S[switch_number].state);		//update state in EEPROM
 		 /*Illustration to write another info regarding S1, say ID into EEPROM:
 		 EEPROM.write(S[switch_number].addr+1, S[switch_number].id); 
 		 assuming id is an element of switches structure and is defined/passed*/
